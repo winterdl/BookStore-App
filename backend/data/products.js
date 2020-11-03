6294f2d63e782a6a22220e6e7197c7582a721c4f
Prince-Shivaram <princeshivaram1999@gmail.com>
2020-11-03 22:57:43 +0530

    ready to deploy

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Python Fluent',
+    image: '/images/python.jpeg',
+    description:
+      'Best Book for learning Python fluently',
+    brand: 'ORIELLY',
+    category: 'Books',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'PHP, MySQL and Web Development',
+    image: '/images/php.jpeg',
+    description:
+      'Learn Web Development using PHP & MySQL',
+      brand: 'ORIELLY',
+      category: 'Books',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Express Web Applications',
+    image: '/images/node3.jpeg',
+    description:
+      'Learn Express JS and build amazing web applications.',
+      brand: 'ORIELLY',
+      category: 'Books',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Node JS Cookbook',
+    image: '/images/node1.png',
+    description:
+      'Learn Node JS from Scratch to Mastery',
+      brand: 'ORIELLY',
+      category: 'Books',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'React JS',
+    image: '/images/react.jpeg',
+    description:
+      'Learn the most popular frontend framework React JS',
+      brand: 'ORIELLY',
+      category: 'Books',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'React Native',
+    image: '/images/react1.jpeg',
+    description:
+      'Learn React Native and build amazing cross-platform Mobile Applications.',
+      brand: 'ORIELLY',
+      category: 'Books',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To BookStore',
+  description: 'We sell the best products for cheap',
+  keywords: 'Books, Programming Books, Web Development Books',
+}
+
+export default Meta
